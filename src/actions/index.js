2a153f0b0e5faba8ebf2bc4c47d12f5fb47a777f
Guilherme Quental <guilherme@quent.al>
2016-11-28 22:23:08 -0200

    Add action to fetch weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios'
+
 const API_KEY = 'bf4588531a3a88740f5edac3b8af817e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appId=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url)
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
