d3861391e68f3d5ed9ff2cebd96512a231d89a9a
Guilherme Quental <guilherme@quent.al>
2016-11-28 23:24:37 -0200

    Add append table list

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    return (
+      <tr key={cityData.city.name}>
+        <td>{cityData.city.name}</td>
+      </tr>
+    )
+  }
+
   render() {
     return (
       <table className="table table-hover">
@@ -14,15 +22,15 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
   }
 }
 
-export default  WeatherList
-
 function mapStateToProps({ weather }) {
   return { weather }
 }
+
+export default connect(mapStateToProps)(WeatherList)
