cc935901b8b9e510dc8034b988d75be2c86cf3d4
Guilherme Quental <guilherme@quent.al>
2016-11-28 22:36:50 -0200

    Add logic to search in API

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchWeather } from '../actions/index'
 
 class SearchBar extends Component {
   constructor(props) {
@@ -12,11 +15,13 @@ class SearchBar extends Component {
 
   onInputChange(event) {
     this.setState({ term: event.target.value })
-    console.log(event.target.value)
   }
 
   onFormSubmit(event) {
     event.preventDefault()
+
+    this.props.fetchWeather(this.state.term)
+    this.setState({ term: '' })
   }
 
   render() {
@@ -36,4 +41,8 @@ class SearchBar extends Component {
   }
 }
 
-export default SearchBar
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
